5f951631f1dbd2ed749e2ec2f43f2e5a9e46e0a5
Chris Cannam <c.cannam@qmul.ac.uk>
2015-09-30 15:08:08 +0100

    Swap a and b buffers (naming only)

--- src/Filter.cpp
+++ src/Filter.cpp
@@ -27,25 +27,25 @@ Filter::Filter(Parameters params)
     m_offmax = 20;
     m_offa = m_offmax;
     m_offb = m_offmax;
-    m_bufa = allocate_and_zero<double>(m_sz + m_offmax);
-    m_bufb = allocate_and_zero<double>(m_order + m_offmax);
+    m_bufa = allocate_and_zero<double>(m_order + m_offmax);
+    m_bufb = allocate_and_zero<double>(m_sz + m_offmax);
 }
 
 Filter::~Filter()
 {
     deallocate(m_a);
     deallocate(m_b);
-    deallocate(m_bufa);
     deallocate(m_bufb);
+    deallocate(m_bufa);
 }
 
 void
 Filter::reset()
 {
-    m_offa = m_offmax;
     m_offb = m_offmax;
-    v_zero(m_bufa, m_sz + m_offmax);
-    v_zero(m_bufb, m_order + m_offmax);
+    m_offa = m_offmax;
+    v_zero(m_bufb, m_sz + m_offmax);
+    v_zero(m_bufa, m_order + m_offmax);
 }
 
 void
@@ -55,24 +55,24 @@ Filter::process(const double *const BQ_R__ in,
 {
     for (int s = 0; s < n; ++s) {
 
-        if (m_offa > 0) --m_offa;
+        if (m_offb > 0) --m_offb;
         else {
-            v_move(m_bufa + m_offmax + 1, m_bufa, m_sz - 1);
-            m_offa = m_offmax;
+            v_move(m_bufb + m_offmax + 1, m_bufb, m_sz - 1);
+            m_offb = m_offmax;
         }
-        m_bufa[m_offa] = in[s];
+        m_bufb[m_offb] = in[s];
 
-	double b_sum = v_multiply_and_sum(m_b, m_bufa + m_offa, m_sz);
-	double a_sum = v_multiply_and_sum(m_a + 1, m_bufb + m_offb, m_order);
+	double b_sum = v_multiply_and_sum(m_b, m_bufb + m_offb, m_sz);
+	double a_sum = v_multiply_and_sum(m_a + 1, m_bufa + m_offa, m_order);
 
 	double outval = b_sum - a_sum;
 
-        if (m_offb > 0) --m_offb;
+        if (m_offa > 0) --m_offa;
         else {
-            v_move(m_bufb + m_offmax + 1, m_bufb, m_order - 1);
-            m_offb = m_offmax;
+            v_move(m_bufa + m_offmax + 1, m_bufa, m_order - 1);
+            m_offa = m_offmax;
         }
-        m_bufb[m_offb] = outval;
+        m_bufa[m_offa] = outval;
         
 	out[s] = outval;
     }
